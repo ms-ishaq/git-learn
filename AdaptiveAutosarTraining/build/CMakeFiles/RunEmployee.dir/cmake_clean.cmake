7b73c03f11d0aa7bc3ec217c35dd388e22f5e021
ms-ishaq <malik-shoaib.ishaq@alten.com>
2022-02-11 02:18:28 +0100

    build folder included

---
+++ AdaptiveAutosarTraining/build/CMakeFiles/RunEmployee.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RunEmployee.dir/EmployeeMain.cpp.o"
+  "RunEmployee"
+  "RunEmployee.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RunEmployee.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AdaptiveAutosarTraining/build/Employee/CMakeFiles/getFunctionObj.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/getFunctionObj.dir/getFunction.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/getFunctionObj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
